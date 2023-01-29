7582ecf606e05c55f68f322d581febe75dafe33d
aderemilekun <adelefatai@yahoo.com>
2023-01-29 18:23:22 +0100

    Implemented Stop watch service

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
+    private var isStarted = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            startOrStop()
+        }
+    }
+
+    private fun startOrStop() {
+        if (isStarted) {
+            stop()
+        } else {
+            start()
+        }
+    }
+
+    private fun start() {
+        binding.btnStart.text = "Stop"
+        isStarted = true;
+    }
+
+    private fun stop() {
+        binding.btnStart.text = "Start"
+        isStarted = false
+    }
+
+    private  fun reset() {
+
     }
 }
